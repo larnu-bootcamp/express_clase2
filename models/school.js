4bf32526136a4326a4524c5cd602468952539dd3
Emiliano Rotta <emilianorottaf@gmail.com>
2022-11-10 12:44:24 -0300

    cambio de nombre school

--- models/school.js
+++ models/school.js
@@ -5,7 +5,7 @@ import db from "../config/database.js";
 // init DataTypes para asignarle los tipos a nuestra base de datos
 const { DataTypes } = Sequelize;
 
-const School = db.define('School', {
+const School = db.define('school', {
 
     id: {
         type: DataTypes.INTEGER, //(asignarle el valor a la base de datos)
